c9fc57b0855045f2ab2804b16f4033b88cfbe6fd
IJOSEY <ijoseybs@GMAIL.COM>
2025-01-23 14:33:38 +0000

    atribuindo valores inicáis !

--- váriaveis.c
+++ váriaveis.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 int main(){
-    int idade;
-    int quantidade;
-    float altura;
-    double peso;
-    char letra;
-    char nome[20];
+    int idade = 25;
+    int quantidade = 1;
+    float altura = 1.75;
+    double peso = 90.5;
+    char letra = 'a';
+    char nome[20] = 'ijosey';
 
 }
