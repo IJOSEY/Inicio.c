b0685be2df96ff4aae3ad24df5d636b94b3ce739
IJOSEY <ijoseybs@GMAIL.COM>
2025-01-23 15:24:02 +0000

    visibilidade

--- váriaveis.c
+++ váriaveis.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 int main(){
-    int idade = 25;
-    int quantidade = 1;
-    float altura = 1.75;
-    double peso = 90.5;
-    char letra = 'a';
-    char nome[20] = 'ijosey';
+    int idade ;
+    int quantidade ;
+    float altura ;
+    double peso ;
+    char letra ;
+    char nome[20] ;
 
     printf("Digite a sua idade:\n");
     scanf("%d",&idade);
