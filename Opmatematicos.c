f3e0cba95cd0da99a20938369ab183aa056ba9c6
IJOSEY <ijoseybs@GMAIL.COM>
2025-01-24 14:33:17 +0000

    menu criado

--- Opmatematicos.c
+++ Opmatematicos.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 int  main (){
     int numero1,numero2,resultado,opcao;
-
-    printf("Opção soma digite: 1\n"); 
+    printf("Opção soma digite: 1\n");
+    printf("Opção subtração digite: 2\n");
+    printf("Opção multiplicação digite: 3\n");
+    printf("Opção divisão digite: 4\n"); 
     printf("Escolha a operação matemática que deseja realizar:");
     scanf("%d",&opcao);
 
