9246dd50c3dc9e9f3178c9a92313755bb06982e6
IJOSEY <ijoseybs@GMAIL.COM>
2025-01-24 14:42:41 +0000

    calculadora simples finalisada!

--- Opmatematicos.c
+++ Opmatematicos.c
@@ -1,20 +1,39 @@
 #include <stdio.h>
 int  main (){
     int numero1,numero2,resultado,opcao;
+
+    printf("Digite o primeiro número:\n");
+    scanf("%d",&numero1);
+    printf("Digite o segundo número:\n");
+    scanf("%d",&numero2);
+    printf("Escolha a operação matemática que deseja realizar:\n");
     printf("Opção soma digite: 1\n");
     printf("Opção subtração digite: 2\n");
     printf("Opção multiplicação digite: 3\n");
     printf("Opção divisão digite: 4\n"); 
-    printf("Escolha a operação matemática que deseja realizar:");
+    
     scanf("%d",&opcao);
 
     switch (opcao)
     {
-    case /* constant-expression */:
-        /* code */
+    case 1 :
+        resultado = numero1 + numero2;
+        printf("A soma dos números é:%d\n", resultado);
         break;
-    
+    case 2 :
+        resultado = numero1 - numero2;
+        printf("A s ubtração dos números é:%d\n", resultado);
+        break;
+    case 3 :
+       resultado = numero1 * numero2;
+        printf("A multiplicação dos números é:%d\n", resultado);
+        break;
+    case 4 :
+        resultado = numero1 / numero2;
+        printf("A divisão dos números é:%d\n", resultado);
+        break;        
     default:
+    printf("Opção invalida!!\n");
         break;
     }
 
