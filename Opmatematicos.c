27d25ee62b26b0273af7aa5c38059849bb959b15
IJOSEY <ijoseybs@GMAIL.COM>
2025-01-24 14:31:24 +0000

    iniciando operadores matemáticos

---
+++ Opmatematicos.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int  main (){
+    int numero1,numero2,resultado,opcao;
+
+    printf("Opção soma digite: 1\n"); 
+    printf("Escolha a operação matemática que deseja realizar:");
+    scanf("%d",&opcao);
+
+    switch (opcao)
+    {
+    case /* constant-expression */:
+        /* code */
+        break;
+    
+    default:
+        break;
+    }
+
+}
