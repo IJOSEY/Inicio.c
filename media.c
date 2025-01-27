3371a5ff6869966b0035373b12cc81f1248b015b
IJOSEY <ijoseybs@GMAIL.COM>
2025-01-27 15:50:25 +0000

    calculo de média finalizado

---
+++ media.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+float main(){
+    float nota1, nota2, media;
+    char nome[20];
+
+    printf("***Calcule sua média***\n");
+
+    printf("Informe seu nome:\n");
+    scanf("%s", &nome);
+
+    printf("Informe 1° nota:\n");
+    scanf("%f", &nota1);
+
+    printf("Informe a 2° nota:\n");
+    scanf("%f", &nota2);
+
+    media = (nota1 + nota2) /2;
+
+    printf("Caro aluno %s a sua média é: %2.f\n",nome,media);
+    printf("");
+    return 0;
+}
