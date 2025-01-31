c37e988b1c11f7b77c597464d6be8909662953e5
IJOSEY <ijoseybs@GMAIL.COM>
2025-01-31 15:36:27 +0000

    finalizado test operadores lógicos!

--- oplogicosteste.c
+++ oplogicosteste.c
@@ -4,18 +4,34 @@ int main (){
      
      printf("Digite um número: \n");
      scanf("%d",&a);
-     printf("Digite outro número: \n");
-     scanf("%d",&b);
+     //printf("Digite outro número: \n");
+     //scanf("%d",&b);
 
 //teste com && lógico
-if (a > 0 && b > 0)
+/*if (a > 0 && b > 0)
 {
     printf("Os números são positívos \n");
 }
 else{
     printf("Pelo menos um dos números não é positivo!\n");
 }
-
-
+*/
+/*Teste com || lógico 
+if(a > 0 || b > 0)
+{
+printf("Pelo menos um dos números é positivo! \n");
+}
+else 
+{
+printf("Ambos números são negattivos! \n");    
+}*/
+if(!(a == 0) )
+{
+   printf("O número é 0! \n");
+}
+else 
+{
+   printf("O é diferente de 0! \n"); 
+}
 return 0;
 }
