3a98d2efd8a0ff30a935bd86238d288699b13a05
IJOSEY <ijoseybs@GMAIL.COM>
2025-02-03 14:12:11 +0000

    aninhada teste !!

---
+++ descontoaninhado.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main () {
+    int idade;
+    float renda;
+   
+    printf("Digite a sua idade: \n");
+    scanf("%d",&idade);  
+
+    printf("Digite sua renda mensal: \n");
+    scanf("%f", &renda);
+
+    if(idade <= 18 || idade >= 60)
+    {
+       printf("Aceito com relação a idade!\n"); 
+     if(renda < 2000)
+    {
+       printf("Você atende aos requisitos do desconto!\n");
+    }
+     else
+    {
+        printf("Você está fora dos requesitos do desconto!\n");
+    }
+    }
+    else
+     {
+       printf("Você não tem idade necessária!\n");
+     }
+    
+    
+    return 0;
+}
